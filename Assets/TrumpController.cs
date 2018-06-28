23baca4a3460db64f6785b1dee118e57283fa4ee
Liu Jie <liujie.king.1023@gmail.com>
2018-06-29 07:31:15 +0800

    Trumpwaking sample on Unity3D

---
+++ Assets/TrumpController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+public class TrumpController : MonoBehaviour {
+
+	private Animation anim;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		float y = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		//transform.position += new Vector3(0,0,y/10);
+		//transform.position += new Vector3(x/10,0,0);
+
+		Vector3 movement = new Vector3 (x, 0.0f, y);
+
+		//enter trumps speed here!!!
+		rb.velocity = movement * 4f;
+
+		if (x != 0 && y != 0) {
+			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, Mathf.Atan2 (x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
+		}
+
+		if (x != 0 || y != 0) {
+			anim.Play ("walk");
+		} else {
+			anim.Play ("idle");
+		}
+	}	
+}
